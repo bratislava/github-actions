eaf652f7ed9d18cf33c5848b8c1c7c978cc57e5f
skifahrer <richard.dvorsky@bratislava.sk>
2024-12-13 13:22:12 +0100

    testing

--- .github/actions/force-ipv4/force-ipv4.sh
+++ .github/actions/force-ipv4/force-ipv4.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Check if script is run with root/sudo privileges
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run with sudo or as root"
-   exit 1
-fi
-
 # Disable IPv6
 echo "Disabling IPv6..."
 sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
