9fb7ed5a0048cc44668852e22709578ac6d739d1
skifahrer <richard.dvorsky@bratislava.sk>
2024-12-13 13:23:09 +0100

    testing

--- .github/actions/force-ipv4/force-ipv4.sh
+++ .github/actions/force-ipv4/force-ipv4.sh
@@ -2,8 +2,8 @@
 
 # Disable IPv6
 echo "Disabling IPv6..."
-sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
-sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1
+sysctl -w net.ipv6.conf.all.disable_ipv6=1
+sysctl -w net.ipv6.conf.default.disable_ipv6=1
 
 # Verify IPv6 status
 echo "Current IPv6 status:"
